0706d2c40d024b014f611280beb7921813b479d6
M1g3l14Ka <katcion00@mail.ru>
2026-05-06 15:11:55 +0300

    change lg:grid-cols-6 -> lg:grid-cols-4

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -43,7 +43,7 @@ export default async function Home({
         </Suspense>
 
         {/* Сетка пользователей */}
-        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 gap-6">
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
           {data.users.length > 0 ? (
             data.users.map((user) => <UserCard key={user.id} user={user} />)
           ) : (
